fffdcf77957efe95f6b9775246d5ea495e7023d0
adeptoftranshumanism <evgeniy@noone.io>
2024-01-12 16:38:52 +0200

    🚚 xprv-xpub moved to noone-android-core-btclike

--- crypto_btc_like/src/main/java/io/noone/androidcore/btclike/networks/BchParams.kt
+++ crypto_btc_like/src/main/java/io/noone/androidcore/btclike/networks/BchParams.kt
@@ -1,6 +1,7 @@
 package io.noone.androidcore.btclike.networks
 
 import io.noone.androidcore.btclike.transaction.HashType
+import io.noone.androidcore.utils.toBytesBE
 
 object BchParams : NetworkParameters() {
     override val dumpedPrivateKeyHeader = 128
@@ -9,4 +10,8 @@ object BchParams : NetworkParameters() {
     override val segwitAddressHrp = "bitcoincash"
     override val hashType: HashType
         get() = HashType.DEFAULT
+    override val slip32PrivKeyPrefix: ByteArray
+        get() = 0x0488ade4.toBytesBE()
+    override val slip32PubKeyPrefix: ByteArray
+        get() = 0x0488b21e.toBytesBE()
 }
--- crypto_btc_like/src/main/java/io/noone/androidcore/btclike/networks/NetworkParameters.kt
+++ crypto_btc_like/src/main/java/io/noone/androidcore/btclike/networks/NetworkParameters.kt
@@ -6,6 +6,8 @@ abstract class NetworkParameters {
     abstract val addressHeader: Int
     abstract val p2SHHeader: Int
     abstract val dumpedPrivateKeyHeader: Int
+    abstract val slip32PubKeyPrefix: ByteArray
+    abstract val slip32PrivKeyPrefix: ByteArray
 
     open val segwitAddressHrp: String? = null
     open val hashType: HashType = HashType.REVERSED
